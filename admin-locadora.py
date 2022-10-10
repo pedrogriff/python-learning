3b51ee7198940cf5fcdce732918eb94618cec2ce
pedrogriff <marcincowski@gmail.com>
2022-10-09 21:43:28 -0300

    versão final do administrador de locação de veículos

--- admin-locadora.py
+++ admin-locadora.py
@@ -16,8 +16,6 @@ def mostrar_lista_de_carros(lista_de_carros):
     for i, car in enumerate(lista_de_carros):
         print("[{}] {} - R$ {} /dia.".format(i, car[0], car[1]))
 
-mostrar_lista_de_carros(carros)
-
 while True:
     os.system("clear")
     print("===========")
@@ -29,13 +27,38 @@ while True:
 
     os.system("clear")
     if op == 0:
-        pass
+        mostrar_lista_de_carros(carros)
 
     elif op == 1:
-        pass
+        mostrar_lista_de_carros(carros)
+        print("==========")
+        print("Escolha o código do carro:")
+        cod_car = int(input())
+        print("Por quantos dias você deseja alugar este carro?")
+        dias = int(input())
+        os.system("clear")
+
+        print("Você escolheu o {} por {} dias.".format(carros[cod_car][0], dias))
+        print("O aluguel totalizaria R$ {}. Deseja alugar?".format(dias * carros[cod_car][1]))
+
+        print("0 - Sim | 1 - Não")
+        conf = int(input())
+        if conf == 0:
+            print("Parabéns você alugou o {} por {} dias.".format(carros[cod_car][0], dias))
+            alugados.append(carros.pop(cod_car))
 
     elif op == 2:
-        pass
+        if len(alugados) == 0:
+            print("Não há carros para devolver.")
+        else:
+            print("Segue a lista de carros alugados. Qual deseja devolver?")
+            mostrar_lista_de_carros(alugados)
+            print("")
+            print("Escolha o código do carro que deseja devolver:")
+            cod = int(input())
+            if conf == 0:
+                print("Obrigado por devolver o carro {}".format(alugados[cod][0]))
+                carros.append(alugados.pop(cod))
 
     print("")
     print("===========")
